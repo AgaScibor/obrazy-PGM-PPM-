f13577d6d7578426604746812e13443e49e25dad
AgaS <116819959+AgaScibor@users.noreply.github.com>
2024-03-26 18:21:40 +0100

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include "PGMReader.hpp"
+#include "PPMReader.hpp"
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Tworzenie obiektu klasy PGMReader
+    PGMReader p("prague.CED.pgm");
+
+    // Wczytanie obrazu i wyświetlenie jego informacji
+    p.printImageInfo();
+
+    // Wyświetlenie najczęściej występującego koloru
+    p.printMostFrequentColor();
+
+    // Obliczenie i wyświetlenie liczby unikalnych kolorów
+    int uniqueColors = p.calcualteNumberOfUniqueColors();
+    cout << "Liczba unikalnych kolorow w obrazie PGM: " << uniqueColors << endl;
+
+    cout << "\n";
+
+    // Tworzenie obiektu klasy PPMReader
+    PPMReader b("blocks.ppm");
+
+    // Wczytanie obrazu PPM i wyświetlenie jego informacji
+    b.printImageInfo();
+
+    // Wyświetlenie najczęściej występującego koloru w obrazie PPM
+    b.printMostFrequentColor();
+
+    // Obliczenie i wyświetlenie liczby unikalnych kolorów w obrazie PPM
+    int uniqueColorsPPM = b.calcualteNumberOfUniqueColors();
+    cout << "Liczba unikalnych kolorow w obrazie PPM: " << uniqueColorsPPM << endl;
+
+    return 0;
+}
