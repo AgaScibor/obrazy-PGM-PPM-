8ad3248c89f56b0b93905286a5fd04f57c87e637
AgaS <116819959+AgaScibor@users.noreply.github.com>
2024-03-26 18:20:41 +0100

    PPMReader.hpp

---
+++ PPMReader.hpp
@@ -0,0 +1,36 @@
+#ifndef _PPMREADER_HPP_
+#define _PPMREADER_HPP_
+
+#include "ImageReader.hpp"
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iostream>
+
+
+// Deklaracja klasy PPMReader dziedziczącej po ImageReader
+class PPMReader : public ImageReader {
+private:
+
+    std::ifstream imageFile;
+    int width;
+    int height;
+    int maxColor;
+    std::vector<std::vector<std::vector<int>>> imageData;
+
+public:
+    // Konstruktor klasy PPMReader
+    PPMReader(std::string imagePath);
+
+    // Metody wirtualne dziedziczone po ImageReader
+    void printImageInfo() override;
+    void printMostFrequentColor() override;
+    int calcualteNumberOfUniqueColors() override;
+    void printImageData() override;
+
+    // Metoda do znajdowania najczęściej występującego koloru
+    std::vector<std::vector<int>> getMostFrequentColors();
+   
+};
+
+#endif // _PPMREADER_HPP_
