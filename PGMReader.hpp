70a2d6a168d088d1285b1fbb3a05636e704883ce
AgaS <116819959+AgaScibor@users.noreply.github.com>
2024-03-26 18:17:23 +0100

    Create PGMReader.hpp

---
+++ PGMReader.hpp
@@ -0,0 +1,28 @@
+#ifndef _PGMREADER_HPP_
+#define _PGMREADER_HPP_
+
+#include "ImageReader.hpp"
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <map>
+
+class PGMReader : public ImageReader {
+private:
+    std::ifstream imageFile;
+    int width;
+    int height;
+    std::vector<int> imageData;
+
+public:
+    PGMReader(std::string imagePath);
+    void printImageInfo() override;
+    void printMostFrequentColor() override;
+    int calcualteNumberOfUniqueColors() override;
+    void printImageData() override;
+
+    std::pair<int, int> getMostFrequentColor();
+};
+
+#endif // _PGMREADER_HPP_
