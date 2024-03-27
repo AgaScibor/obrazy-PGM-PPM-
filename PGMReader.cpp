a34ac54ad450daf7fb316f46ea6d3456cf87ed12
AgaS <116819959+AgaScibor@users.noreply.github.com>
2024-03-27 12:30:51 +0100

    PGMReader.cpp

--- PGMReader.cpp
+++ PGMReader.cpp
@@ -6,90 +6,106 @@
 #include <set>
 #include <map>
 
-PGMReader::PGMReader(std::string imagePath) : width(0), height(0) {
+
+using namespace std;
+
+PGMReader::PGMReader(string imagePath) : width(0), height(0) 
+{
     imageFile.open(imagePath);
-    if (imageFile.good()) {
-        std::string fileType;
-        std::string line;
+    if (imageFile.good()) 
+    {
+        string fileType;
+        string line;
         int pixelValue;
 
         try {
             // Read the magic number
             getline(imageFile, fileType);
-            if (fileType == "P2") {
+            if (fileType == "P2") 
+            {
                 // Ignore comments
                 while (getline(imageFile, line)) {
-                    if (line.empty() || line[0] != '#') {
-                        std::istringstream iss(line);
+                    if (line.empty() || line[0] != '#')
+                    {
+                        istringstream iss(line);
                         iss >> width >> height;
                         break;
                     }
                 }
 
                 // Read pixel data
-                for (int i = 0; i < width * height; ++i) {
+                for (int i = 0; i < width * height; ++i)
+                {
                     imageFile >> pixelValue;
                     imageData.push_back(pixelValue);
                 }
             }
-            else {
-                std::cout << "Plik jest innego typu niż PGM: " << imagePath << "\n";
+            else 
+            {
+                cout << "Plik jest innego typu niz PGM: " << imagePath << "\n";
             }
         }
-        catch (std::exception& e) {
-            std::cout << e.what() << "\n";
+        catch (std::exception& e) 
+        {
+            cout << e.what() << "\n";
         }
 
     }
-    else {
-        std::cout << "Nie można otworzyć pliku: " << imagePath << "\n";
+    else 
+    {
+        cout << "Nie mozna otworzyc pliku: " << imagePath << "\n";
     }
 }
 
-void PGMReader::printImageInfo() {
-    std::cout << "Rozmiar obrazu: " << width << "x" << height << "\n";
-    std::cout << "Unikalnych kolorów: " << calcualteNumberOfUniqueColors() << "\n";
+void PGMReader::printImageInfo() 
+{
+    cout << "Rozmiar obrazu: " << width << "x" << height << "\n";
+    cout << "Unikalnych kolorow: " << calcualteNumberOfUniqueColors() << "\n";
     printMostFrequentColor();
-    printImageData();
+
+    // Wyświetlanie unikalnych kolorów
+    set<int> uniqueColors(imageData.begin(), imageData.end());
+    cout << "Unikalne kolory:";
+    for (int color : uniqueColors) 
+    {
+        cout << " " << color;
+    }
+    cout << "\n";
 }
 
-void PGMReader::printMostFrequentColor() {
+void PGMReader::printMostFrequentColor() 
+{
     auto mostFrequentColor = getMostFrequentColor();
-    std::cout << "Najczęstszy kolor: " << mostFrequentColor.first << " (Ile razy: " << mostFrequentColor.second << ")\n";
+    cout << "Najczestszy kolor: " << mostFrequentColor.first << " (Ile razy: " << mostFrequentColor.second << ")\n";
 }
 
-int PGMReader::calcualteNumberOfUniqueColors() {
-    std::set<int> uniqueColors(imageData.begin(), imageData.end());
+int PGMReader::calcualteNumberOfUniqueColors() 
+{
+    set<int> uniqueColors(imageData.begin(), imageData.end());
     return uniqueColors.size();
 }
 
-std::pair<int, int> PGMReader::getMostFrequentColor() {
-    std::map<int, int> colorFrequency;
+pair<int, int> PGMReader::getMostFrequentColor() 
+{
+    map<int, int> colorFrequency;
 
     // Obliczanie częstości występowania kolorów
-    for (int color : imageData) {
+    for (int color : imageData)
+    {
         colorFrequency[color]++;
     }
 
     // Znajdowanie koloru o najwyższej częstości
     int maxFrequency = 0;
     int mostFrequentColor = 0;
-    for (const auto& pair : colorFrequency) {
-        if (pair.second > maxFrequency) {
+    for (const auto& pair : colorFrequency) 
+    {
+        if (pair.second > maxFrequency) 
+        {
             maxFrequency = pair.second;
             mostFrequentColor = pair.first;
         }
     }
 
-    return std::make_pair(mostFrequentColor, maxFrequency);
-}
-
-void PGMReader::printImageData() {
-    std::set<int> uniqueColors(imageData.begin(), imageData.end());
-
-    std::cout << "Unikalne kolory na obrazie:\n";
-    for (int color : uniqueColors) {
-        std::cout << color << " ";
-    }
-    std::cout << "\n";
+    return make_pair(mostFrequentColor, maxFrequency);
 }
