307262b239c193b2f0d732e612a32747e55fd334
AgaS <116819959+AgaScibor@users.noreply.github.com>
2024-03-27 17:49:36 +0100

    ImageReader.hpp

--- ImageReader.hpp
+++ ImageReader.hpp
@@ -9,7 +9,7 @@ public:
 
     virtual void printImageInfo() = 0;
     virtual void printMostFrequentColor() = 0;
-    virtual int calcualteNumberOfUniqueColors() = 0;
+    virtual int calculateNumberOfUniqueColors() = 0;
 };
 
 #endif // _IMAGE_READER_HPP_
